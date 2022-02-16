8351c86b70ed70d2d2bf3b6cb8644d8413a7574f
WinstonNguyen <winstonnguyen2317@gmail.com>
2022-02-16 00:24:34 -0600

    ForLoop

---
+++ For Loop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ForLoop
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine("value of i: {0}", i);
+            }
+        }
+    }
+}
