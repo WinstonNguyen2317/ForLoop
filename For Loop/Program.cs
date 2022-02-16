6ea5af92610c8867f77710277c2eeb20cd82b8e7
WinstonNguyen <winstonnguyen2317@gmail.com>
2022-02-16 00:30:07 -0600

    Forloop edit

--- For Loop/Program.cs
+++ For Loop/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 101; i++)
+            for (int i = 1; i < 101; i++)
             {
-                Console.WriteLine("value of i: {0}", i);
+                Console.WriteLine(i);
             }
         }
     }
